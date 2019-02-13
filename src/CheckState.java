bf638ac2965b2109e8435e9802f59e600e4281e0
Aida Sabaghmoghadam <aida.sabbagh@gmail.com>
2019-02-13 13:52:24 -0500

    correct the parameter of main method

--- src/CheckState.java
+++ src/CheckState.java
@@ -2,7 +2,7 @@ import java.util.Random;
 
 public class CheckState {
 
-    public static void main(String args){
+    public static void main(String[] args){
 
         Random random = new Random();
         boolean isCapital = random.nextBoolean();
