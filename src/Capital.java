c56d0458a35e5175848e78fb6bc15924adaadae0
Aida Sabaghmoghadam <aida.sabbagh@gmail.com>
2019-02-13 13:26:07 -0500

    change the constructor of Capital

--- src/Capital.java
+++ src/Capital.java
@@ -1,6 +1,8 @@
 public class Capital extends City {
 
-    private boolean isCapital ;
+    public Capital(int numberOfCitizens, double taxesPerCitizen) {
+        super(numberOfCitizens, taxesPerCitizen);
+    }
 
     public boolean isMeropolis(){
         return getNumberOfCitizens() > 100000 && getNumberOfCitizens() > 720000000.0 ;
